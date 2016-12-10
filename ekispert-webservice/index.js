e3824c48ce551fb787722f1eff7c4eb7122efc76
naokikimura <n.kimura.cap@gmail.com>
2016-12-10 22:42:53 +0900

    Update ekispert-webservice/index.html

--- ekispert-webservice/index.js
+++ ekispert-webservice/index.js
@@ -1,5 +1,8 @@
 $(function () {
-  $("#date").val((new Date()).toISOString().substring(0, 10));
+  var now = new Date();
+  $("#date").html(now.toLocaleDateString()).attr("datetime", now.toISOString().substring(0, 10));
+
+  $('<figure id="result" title="経路検索結果"><iframe src="about:blank" frameborder="0"></iframe></figure>').appendTo("body");
   $("#result").dialog({
     autoOpen: false,
     width: "auto",
@@ -8,8 +11,8 @@ $(function () {
   });
   $("#result iframe").on("load", function (e) {
     console.log("load");
-    $("#result").dialog("open");
   });
+
   $(".amount").after('<button type="button" class="search"><img src="./icon_150050.svg" alt="経路検索" /></button>');
   $(".search").on("click", function (e) {
     console.log("search");
@@ -20,8 +23,8 @@ $(function () {
 
     var params = {
       "key": "LE_aYYhp8kwYt3Yb",
-      "date": $("#date").val().replace(/\D/g, ""),
-      "time": "0900",
+      "date": $("#date").attr("datetime").replace(/\D/g, ""),
+      "time": ("00" + Number($(".beginning .hour").val())).slice(-2) + ("00" + Number($(".beginning .minute").val())).slice(-2),
       "from": $(this).parents("tr").find(".from").val(),
       "to": $(this).parents("tr").find(".to").val(),
       "contentsMode": "sp"
@@ -35,6 +38,7 @@ $(function () {
       console.log(response);
       var src = response.ResultSet.ResourceURI.replace(/^http:/, "https:");
       $("#result iframe").attr("src", src);
+      $("#result").dialog("open");
     }).fail(function (xhr, statuText, err) {
       var response = xhr.responseJSON || JSON.parse(xhr.responseText);
       alert(response.ResultSet.Error.Message);
