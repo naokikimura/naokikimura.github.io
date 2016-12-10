5ebd427545b85785afa9ccc811b4eade9d1cd51d
naokikimura <n.kimura.cap@gmail.com>
2016-12-10 13:39:50 +0900

    Update ekispert-webservice/index.html

--- ekispert-webservice/index.js
+++ ekispert-webservice/index.js
@@ -1,47 +1,38 @@
-window.onload = function () {
-  document.getElementById("date").value = new Date().toISOString().substring(-1, 10);
-  var figure = document.getElementById("result");
-  var iframe = figure.querySelector("iframe");
-  var a = figure.querySelector(".close");
-  a.addEventListener("click", function (e) {
-    e.preventDefault();
-    figure.classList.add("hidden");
+$(function () {
+  $("#date").val((new Date()).toISOString().substring(0, 10));
+  $("#result").dialog({
+    autoOpen: false,
+    width: "auto"
   });
-  iframe.addEventListener("load", function (e) {
-    figure.classList.remove("hidden");
+  $("#result iframe").on("load", function (e) {
+    console.log("load");
+    $("#result").dialog("open");
   });
-  var button = document.getElementById("search");
-  button.addEventListener("click", function (e) {
+  $("#search").on("click", function (e) {
     console.log("search");
-    figure.classList.add("hidden");
-    var toQueryString = function (params) {
-      return Object.keys(params).map(function (e) { return [e, params[e]]; })
-        .reduce(function (p, c) { return p + "&" + c[0] + "=" + encodeURIComponent(c[1]); }, "?");
-    };
+    $("#result").dialog("close");
+    $("#result").dialog({
+      position: { my: "right top", at: "right bottom", of: this }
+    });
+
     var params = {
       "key": "LE_aYYhp8kwYt3Yb",
-      "date": document.getElementById("date").value.replace(/\D/g, ""),
+      "date": $("#date").val().replace(/\D/g, ""),
       "time": "0900",
-      "from": document.getElementById("from").value,
-      "to": document.getElementById("to").value,
+      "from": $("#from").val(),
+      "to": $("#to").val(),
       "contentsMode": "sp"
     };
-    var url = "https://api.ekispert.jp/v1/json/search/course/light" + toQueryString(params);
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.responseType = "json";
-    xhr.onload = function(e) {
-      console.log(this.response);
-      var response = typeof this.response == "string" ? JSON.parse(this.response) : this.response;
-      if (this.status == 200) {
-        console.log(response.ResultSet.ResourceURI);
-        // var win = window.open(response.ResultSet.ResourceURI);
-        iframe.src = response.ResultSet.ResourceURI.replace(/^http:/, "https:");
-      } else if (this.status == 400) {
-        alert(response.ResultSet.Error.Message);
-      }
-    };
-    xhr.send();
+    $.get({
+      crossDomain: true,
+      url: "https://api.ekispert.jp/v1/json/search/course/light?" + $.param(params),
+      dataType: "json"
+    }).done(function (response) {
+      console.log(response);
+      $("#result iframe").attr("src", response.ResultSet.ResourceURI.replace(/^http:/, "https:"));
+    }).fail(function (xhr, statuText, err) {
+      var response = xhr.responseJSON || JSON.parse(xhr.responseText);
+      alert(response.ResultSet.Error.Message);
+    });
   });
-}
+});
